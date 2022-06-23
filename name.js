291555596e230bc51ee78a9de45855c1fcb902fa
Ridwan Sulaiman <105246901+surereal4@users.noreply.github.com>
2022-06-23 11:56:03 +0100

    First push

---
+++ name.js
@@ -0,0 +1,7 @@
+// let myName = 'Ridwan Ishola Sulaiman'
+// let height = 1.5
+// let country = 'Nigeria'
+// document.write(myName);
+// document.write(height);
+// document.write(country);
+document.write('Sulaiman, Ridwan Ishola <br/> 1.5 <br/> Nigeria')
